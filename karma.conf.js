5b066dc2b48c591a15b1f196252e55940e5b4770
huk11 <hammadullahkhan@gmail.com>
2020-08-21 04:27:12 -0400

    github pages 2

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/peachtree-bank'),
+      dir: require('path').join(__dirname, './coverage/leader-board'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'peachtree-bank'`, () => {
+  it(`should have as title 'leader-board'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('peachtree-bank');
+    expect(app.title).toEqual('leader-board');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('peachtree-bank app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('leader-board app is running!');
   });
 });
